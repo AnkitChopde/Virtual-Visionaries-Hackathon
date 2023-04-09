cb1593b33c518b21dcea977b0d486c266a1798ab
yash <yash10072000@gmail.com>
2023-04-09 15:23:09 +0530

    added home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 
 import './App.css';
 import SnakeLadder from "../src/Components/SnankLadder"
+import Home from './Components/Home';
 function App() {
   return (
     <div className="App">
-      <SnakeLadder/>
+      {/* <SnakeLadder/> */}
+      <Home/>
     </div>
   );
 }
