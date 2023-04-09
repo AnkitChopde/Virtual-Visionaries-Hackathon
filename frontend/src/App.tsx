1fb92a7a89fe8297a245c9bb3fac4dc84589df88
Nitin Kondhari <nitinkondhari85@gmail.com>
2023-04-09 15:09:37 +0530

    add this snake

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 
 import './App.css';
-import SnakeLadder from "../src/Components/SnankLadder"
+
 function App() {
   return (
     <div className="App">
-      <SnakeLadder/>
+    
     </div>
   );
 }
+export default App
 
-export default App;
+
