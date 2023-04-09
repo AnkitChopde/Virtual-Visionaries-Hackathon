954deec8a5ff44614cbf95a16a9428381f17a540
Ankesh Chopde <acchopde84@gmail.com>
2023-04-09 09:33:36 +0530

    login/signup

--- Backend/index.js
+++ Backend/index.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const connection = require("./configs/db");
-
+const userRoutes = require("./routes/user.routes");
+require("dotenv").config();
 const app = express();
-
 app.use(express.json());
+app.use("/user",userRoutes)
+
+
 
 app.get("/",(req,res)=>{
     res.send("Home Page")
 });
 
-app.listen(process.env.mongoURL,async()=>{
+
+app.listen(process.env.port,async()=>{
   try{
     await connection
     console.log("connected to database")
--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     email:String,
-    nickName:String,
+    username:String,
+    password:String,
     coins:Number,
 });
 
+const UserModel = mongoose.model("user",userSchema);
+
+module.exports=UserModel;
